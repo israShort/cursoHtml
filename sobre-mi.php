e142937a809066247d828f457056416f1cfe7516
israShort <israshort@gmail.com>
2020-09-18 19:07:07 -0300

    PHP

--- sobre-mi.php
+++ sobre-mi.php
@@ -26,17 +26,17 @@
                         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                             <ul class="navbar-nav mr-auto mb-2 mb-sm-0">
                                 <li class="nav-item pr-5 pl-2 pl-sm-0">
-                                    <a class="nav-link text-left text-sm-center pt-1" href="index.html">Home <span
+                                    <a class="nav-link text-left text-sm-center pt-1" href="index.php">Home <span
                                             class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item pr-5 pl-2 pl-sm-0">
-                                    <a class="nav-link active text-center text-sm-center pt-1" href="sobre-mi.html">Sobre mi</a>
+                                    <a class="nav-link active text-center text-sm-center pt-1" href="sobre-mi.php">Sobre mi</a>
                                 </li>
                                 <li class="nav-item pr-5 pl-2 pl-sm-0">
-                                    <a class="nav-link text-left text-sm-center pt-1" href="proyectos.html">Proyectos</a>
+                                    <a class="nav-link text-left text-sm-center pt-1" href="proyectos.php">Proyectos</a>
                                 </li>
                                 <li class="nav-item pr-5 pl-2 pl-sm-0">
-                                    <a class="nav-link text-left text-sm-center pt-1" href="contacto.html">Contacto</a>
+                                    <a class="nav-link text-left text-sm-center pt-1" href="contacto.php">Contacto</a>
                                 </li>
                             </ul>
                             <a href="files/CV_Israel_Short.pdf" target="_blank" class="descarga-cv py-1 px-3">DESCARGÁ MI CV <i class="fas fa-arrow-down"></i></a>
