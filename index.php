c1d53a74425457e4455b34853455eae35889d253
israShort <70781093+israShort@users.noreply.github.com>
2020-09-23 11:07:19 -0300

    Include2

--- index.php
+++ index.php
@@ -43,21 +43,7 @@
         </div>
     </main>
     <footer class="container px-sm-0">
-        <div class="row text-center text-sm-left">
-            <div class="col-sm-2 col-12 text-sm-left text-center ml-sm-3 pl-0 pr-1 pr-sm-0 iconos">
-                <a href="https://github.com/israShort" target="_blank" title="Github"><i class="fab fa-github m-2 m-sm-1"></i></a>
-                <a href="https://www.linkedin.com/in/israel-short-4b3779148/" target="_blank" title="Linkedin"><i class="fab fa-linkedin m-2 m-sm-1"></i></a>
-            </div>
-            <div class="col-sm-3 col-12 text-sm-left text-center mt-2 mt-sm-0">
-                <a href="https://depcsuite.com/?v=d72a48a8ebd2" target="_blank" title="Whatsapp">Sponsor <span class="destacado">DePC Suite</span></a>
-            </div>
-            <div class="col-sm-3 col-12 text-sm-left text-center mt-2 mt-sm-0">
-                <a href="mailto:info@depcsuite.com" target="_blank" title="Mail">info@depcsuite.com</a>
-            </div>
-            <div class="col-sm-4 col-12 text-center text-sm-right">
-                <a href="https://wa.me/5491122543823" target="_blank" title="Whatsapp"><i class="fab fa-whatsapp pt-3 pb-3 pb-sm-5 pr-4 pl-4"></i></a>
-            </div>
-        </div>
+        <?php include_once("footer.php"); ?>
     </footer>
 </body>
 
