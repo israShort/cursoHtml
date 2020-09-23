8e88ca45399f99c29b74a36ec2c1e4d419f702c9
israShort <70781093+israShort@users.noreply.github.com>
2020-09-23 08:55:46 -0300

    prueba

--- proyectos.php
+++ proyectos.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyectos | Mi Portfolio</title>
+    <title>Proyectos | Mi Portfolioo</title>
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
